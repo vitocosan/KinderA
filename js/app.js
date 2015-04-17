b4eba8dd044c187e06c29282e5d4e9cfd22e0ac9
Victor Varela <vvarela@ceicdata.com>
2015-04-17 17:16:50 -0300

    new directives for menu and footer

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@
   app.directive("headerOptions", function(){
     return {
       restrict: 'E',
-      templateUrl: "js/header-options.html",
+      templateUrl: "directive/header-options.html",
       scope:{
         menu: "="
       },
@@ -34,5 +34,39 @@
       }
     };
   });
+
+  app.directive("sectionFooter", function(){
+    return{
+      restrict: 'E',
+      templateUrl: "directive/section-footer.html",
+      controllerAs: 'MenuOptions',
+      controller: function(){
+        this.menu_items = menu_options;
+      }
+    };
+  });
+
+  var menu_options = [
+    {
+        "name": "Home",
+        "url":"index.html"
+    },
+    {
+        "name": "Quienes Somos",
+        "url":"about.html"
+    },
+    {
+        "name": "Galeria",
+        "url":"gallery.html"
+    },
+    {
+        "name": "Blog",
+        "url":"blog.html"
+    },
+    {
+        "name": "Contacto",
+        "url":"contact.html"
+    }
+  ];
 
 })();
