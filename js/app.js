f3dff1355d3ec85b285c60cd058dc43d4972c032
Victor Varela <vvarela@ceicdata.com>
2015-04-21 11:31:25 -0300

    ueva informacion y pagina de tesoreria

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 (function(){
-  var app = angular.module('kinder', []);
+  var app = angular.module('kinder', ['smart-table']);
 
-  app.controller("BulletController", function() {
-    this.bullets = image_bullets;
-  });
-
+  app.controller('horarioController', ['$scope', function ($scope) {
+      $scope.rowCollection = [
+          {lunes: 'Saludo', martes: 'Saludo', miercoles: 'Saludo', jueves: 'Saludo', viernes: 'Saludo'},
+          {lunes: 'Lectura Diaria', martes: 'Lectura Diaria', miercoles: 'Lectura Diaria', jueves: 'Lectura Diaria', viernes: 'Lectura Diaria'},
+          {lunes: 'Ingles', martes: 'Ingles', miercoles: 'Ingles', jueves: 'Computación', viernes: 'Ingles'},
+          {lunes: 'Colación', martes: 'Colación', miercoles: 'Colación', jueves: 'Colación', viernes: 'Colación'},
+          {lunes: 'Patio', martes: 'Patio', miercoles: 'Patio', jueves: 'Patio', viernes: 'Patio'},
+          {lunes: 'Ed Fisica', martes: 'Trabajo Personal', miercoles: 'Ed Física', jueves: 'Trabajo Pesonal', viernes: 'Trabajo Pesonal'},
+          {lunes: 'Colación', martes: 'Colación', miercoles: 'Colación', jueves: 'Colación', viernes: 'Biblioteca'},
+          {lunes: 'Patio', martes: 'Patio', miercoles: 'Patio', jueves: 'Patio', viernes: 'Patio'},
+          {lunes: 'Trabajo Personal', martes: 'Estaciones', miercoles: 'Trabajo Personal', jueves: 'CREM', viernes: 'Estaciones'}
+      ];
+  }]);
 
   app.directive("headerOptions", function(){
     return {
@@ -38,51 +47,5 @@
     };
   });
 
-  var menu_options = [
-    {
-        "name": "Home",
-        "url":"index.html"
-    },
-    {
-        "name": "Quienes Somos",
-        "url":"about.html"
-    },
-    {
-        "name": "Galeria",
-        "url":"gallery.html"
-    },
-    {
-        "name": "Blog",
-        "url":"blog.html"
-    },
-    {
-        "name": "Contacto",
-        "url":"contact.html"
-    }
-  ];
-
-  var image_bullets = [
-  	{
-  		"src" : "images/wowslider/01.png",
-  		"alt" : "This is the title",
-  		"title" : "This is the title",
-  		"id" : "wows1_0",
-  		"description" : "And this is description"
-  	},
-  	{
-  		"src" : "images/wowslider/02.png",
-  		"alt" : "Another title",
-  		"title" : "Another title",
-  		"id" : "wows1_1",
-  		"description" : "And another description"
-  	},
-  	{
-  		"src" : "images/wowslider/03.png",
-  		"alt" : "Third title",
-  		"title" : "With third description",
-  		"id" : "wows1_2",
-  		"description" : "And another description"
-  	}
-  ];
 
 })();
